603b9f0ae7594df2d684f6de3b7bf0d1dfa1068a
aastharawat <aastha.rawat33@gmail.com>
2020-02-16 14:41:18 +0530

    Time input

--- src/components/VideoSnapshot.jsx
+++ src/components/VideoSnapshot.jsx
@@ -1,6 +1,6 @@
 import React, { createRef, useState } from "react";
 import ReactPlayer from "react-player";
-import { Button, Box } from "@material-ui/core";
+import { Button, Box, TextField } from "@material-ui/core";
 
 export function VideoCanvas() {
   let videoRef = createRef();
@@ -8,66 +8,76 @@ export function VideoCanvas() {
   let imageRef = createRef();
   let linkRef = createRef();
   const [duration, setDuration] = useState();
-  const [snapshotTime, setsnapshotTime] = useState();
+  const [seconds, setSeconds] = useState(0);
 
-  const captureFrame = () => {
-    const video = videoRef.current
-    const videoPlayer = video.getInternalPlayer();
-    const canvas = canvasRef.current;
-    canvas.width = videoPlayer.videoWidth;
-    canvas.height = videoPlayer.videoHeight;
-        canvas.getContext("2d").drawImage(videoPlayer, 0, 0);
-        let dataUri = canvas.toDataURL("image/" + "jpeg");
-        // Setting image
-        linkRef.current.href = dataUri;
-        linkRef.current.download = "screenshot";
-        imageRef.current.src = dataUri;
+const captureFrame = () => {
+  let video = videoRef.current.getInternalPlayer();
+  let canvas = canvasRef.current;
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
 
-      setsnapshotTime(video.getCurrentTime());
-      setDuration(video.getDuration());
-  };
+  videoRef.current.seekTo(seconds, "seconds");
+  setTimeout(() => {
+    canvas.getContext("2d").drawImage(video, 0, 0);
+    let dataUri = canvas.toDataURL("image/jpeg");
 
-  return (
-    <>
-      <Box
-        style={{ marginTop: 30 }}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <ReactPlayer
-          id="video"
-          ref={videoRef}
-          url="https://cdn.rawgit.com/mediaelement/mediaelement-files/4d21a042/big_buck_bunny.mp4"
-          controls
-          onStart={captureFrame}
-          config={{
-            file: {
-              attributes: {
-                crossOrigin: "anonymous"
-              }
+    //Setting image
+    linkRef.current.href = dataUri;
+    linkRef.current.download = "screenshot";
+    imageRef.current.src = dataUri;
+    setDuration(videoRef.current.getDuration());
+  }, 500);
+};
+
+return (
+  <>
+    <Box
+      style={{ marginTop: 30 }}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <ReactPlayer
+        id="video"
+        ref={videoRef}
+        url="Uploads/SampleVideo_1280x720_1mb.mp4"
+        controls
+        config={{
+          file: {
+            attributes: {
+              crossOrigin: "anonymous"
             }
-          }}
-        />
-        <div>
-          <b> Duration of the video in seconds: </b>
-          {duration}
-        </div>
-          <div>
-          <b>Time at which snapshot was taken(seconds): </b> {snapshotTime}
-
-          </div>
-        <div style={{ marginTop: 30 }}>
-          <b>Click on the Screenshot to download it:</b>
-        </div>
-        <a href="javascript;" id="link" ref={linkRef}>
-          <img id="img" ref={imageRef} src="" alt="screenshot" />
-        </a>
-        <canvas ref={canvasRef} style={{ display: "none" }} alt="" />
-      </Box>
-    </>
-  );
+          }
+        }}
+      />
+      <div>
+        <b> Duration of the video in seconds: </b>
+        {duration}
+      </div>
+      <TextField
+        id="Seconds"
+        label="Seconds"
+        type="number"
+        onChange={e => setSeconds(e.target.value)}
+      />
+      <Button
+        style={{ marginTop: 30 }}
+        variant="contained"
+        color="secondary"
+        onClick={captureFrame}
+      >Take snapshot</Button>
+      <div style={{ marginTop: 30 }}>
+        <b>Click on the Screenshot to download it:</b>
+      </div>
+      <a href="javascript;" id="link" ref={linkRef}>
+        <img id="img" ref={imageRef} src=""/>
+      </a>
+      <canvas ref={canvasRef} style={{ display: "none" }} alt="" />
+    </Box>
+  </>
+);
 }
 
 export default VideoCanvas;
+
